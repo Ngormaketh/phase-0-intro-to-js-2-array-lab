23d28ead63476b2984b2e58f0b2e2da0415d76c6
Anyang Diing <anyang.ngor@student.moringaschool.com>
2024-12-29 18:03:35 +0300

    Introduction to Arrays in Js

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution here!
+// Initial cats array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive Functions
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-Destructive Functions
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
+
